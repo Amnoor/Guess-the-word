93ee75adba988611899bf2f6ad9ac45ef9ee4250
Amnoor Brar <TechMan1810@gmail.com>
2025-09-13 19:41:15 +0800

    Update choices in the guess the word game to use letters and fixed the answer checking.

--- Guess the word.py
+++ Guess the word.py
@@ -2,23 +2,23 @@
 # First I will import the random module to randomly select a word from the list of choices.
 import random
 # Then I will create a list of 10 choices.
-choices = ["Apple", "Phone", "Chair", "Bread", "Window", "Street", "Paper", "Cloud", "Light", "Door"]
+choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 # Then I will randomly select a word from the list of choices.
 Answer = random.choice(choices)
 # Then I will Welcome the user to the game and explain the rules.
 print("Welcome to guess the word!")
 print("You will be given 10 words you have to choose from and you have 9 tries to guess the correct word. one round only.")
 # Then I will display the list of choices to the user.
-print(f"""a. {choices[0]}
-b. {choices[1]}
-c. {choices[2]}
-d. {choices[3]}
-e. {choices[4]}
-f. {choices[5]}
-g. {choices[6]}
-h. {choices[7]}
-i. {choices[8]}
-j. {choices[9]}""")
+print(f"""{choices[0]}. Apple
+{choices[1]}. Phone
+{choices[2]}. Chair
+{choices[3]}. Bread
+{choices[4]}. Window
+{choices[5]}. Street
+{choices[6]}. Paper
+{choices[7]}. Cloud
+{choices[8]}. Light
+{choices[9]}. Door""")
 # Then I will ask the user to enter their guess.
 guess = input("Enter your guess (letters only): ")
 # Then I will check if the user's input is valid (letters only).
